33e696f636a17ecf85c4d5d12e5ee2e7d0bac1d3
KaterynaBachkalo <kozlovets88@gmail.com>
2024-09-30 15:41:52 +0300

    add script for hiding table

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,45 +1,41 @@
-(() => {
-  const mobileMenu = document.querySelector(".js-menu");
-  const openMenuBtn = document.querySelector(".js-menu-open");
-  const closeMenuBtn = document.querySelector(".js-menu-close");
-  const menuBtn = document.querySelectorAll(".menu-link");
+const mobileMenu = document.querySelector(".js-menu");
+const openMenuBtn = document.querySelector(".js-menu-open");
+const closeMenuBtn = document.querySelector(".js-menu-close");
+const menuBtn = document.querySelectorAll(".menu-link");
+const sidebar = document.querySelector(".js-menu");
+const tableWrap = document.querySelector(".table-data");
+const table = document.querySelector(".table");
 
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
-    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-    mobileMenu.classList.toggle("is-open");
-  };
+const toggleMenu = () => {
+  const isMenuOpen =
+    openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+  openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+  mobileMenu.classList.toggle("is-open");
+};
 
-  openMenuBtn.addEventListener("click", toggleMenu);
-  closeMenuBtn.addEventListener("click", toggleMenu);
-  menuBtn.forEach((item) => {
-    item.addEventListener("click", toggleMenu);
-  });
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
+menuBtn.forEach((item) => {
+  item.addEventListener("click", toggleMenu);
+});
 
-  window.matchMedia("(min-width: 1440px)").addEventListener("change", (e) => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove("is-open");
-    openMenuBtn.setAttribute("aria-expanded", false);
-  });
-})();
+window.matchMedia("(min-width: 1440px)").addEventListener("change", (e) => {
+  if (!e.matches) return;
+  mobileMenu.classList.remove("is-open");
+  openMenuBtn.setAttribute("aria-expanded", false);
+});
 
-// // Отримуємо елементи
-// const sidebar = document.querySelector(".sidebar");
-// const body = document.querySelector(".page");
-// const main = document.querySelector(".main");
-// const openMenuBtn = document.querySelector(".js-menu-open");
-// const closeMenuBtn = document.querySelector(".js-menu-close");
+const openSidebar = () => {
+  sidebar.classList.add("is-open");
+  tableWrap.classList.add("hidden");
+  table.classList.add("hidden");
+};
 
-// function toggleSidebar() {
-//   if (sidebar.classList.contains("is-open")) {
-//     // body.classList.add("body-no-scroll");
-//     main.classList.add("body-no-scroll");
-//   } else {
-//     // body.classList.remove("body-no-scroll");
-//     main.classList.remove("body-no-scroll");
-//   }
-// }
+const closeSidebar = () => {
+  sidebar.classList.remove("is-open");
+  tableWrap.classList.remove("hidden");
+  table.classList.remove("hidden");
+};
 
-// closeMenuBtn.addEventListener("click", toggleSidebar);
-// openMenuBtn.addEventListener("click", toggleSidebar);
+openMenuBtn.addEventListener("click", openSidebar);
+closeMenuBtn.addEventListener("click", closeSidebar);
