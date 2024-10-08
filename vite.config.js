a7eae95b13d588461cad45a3eac790f6c8df78ed
KaterynaBachkalo <kozlovets88@gmail.com>
2024-10-08 20:26:25 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -61,6 +61,6 @@ export default defineConfig(({ command }) => {
       }),
     ],
 
-    base: command === "serve"? "/customers.html": "/crm-dashboard-customers-pug/customers.html",
+    base: "/customers.html",
   };
 });
