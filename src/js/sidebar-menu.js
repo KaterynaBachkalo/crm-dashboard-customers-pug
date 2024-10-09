18231ad0efa5cfa422906284cc8574a4d28342fc
KaterynaBachkalo <kozlovets88@gmail.com>
2024-10-09 12:12:58 +0300

    delete consoles

--- src/js/sidebar-menu.js
+++ src/js/sidebar-menu.js
@@ -2,7 +2,6 @@ const menuItems = document.querySelectorAll(".menu-link");
 
 
 const currentPath = window.location.pathname;
-console.log("currentPath:", currentPath);
 
 let updateCurrentPath;
 
@@ -12,14 +11,11 @@ if (currentPath.includes("/crm-dashboard-customers-pug/")) {
   updateCurrentPath = currentPath;
 }
 
-console.log("updateCurrentPath:", updateCurrentPath);
-
 
 const checkCurrentMenuLink = () => {
   menuItems.forEach((item) => {
 
     const clickPath = item.getAttribute("href").replace('./', '/');
-    console.log("clickPath1:", clickPath);
 
     if (clickPath === updateCurrentPath) {
       item.classList.add("current"); 
@@ -34,7 +30,6 @@ document.addEventListener("DOMContentLoaded", checkCurrentMenuLink);
 menuItems.forEach((item) => {
   item.addEventListener("click", (e) => {   
     const clickPath = item.getAttribute("href").replace('./', '/');
-    console.log("clickPath2:", clickPath);
 
     if (updateCurrentPath === clickPath) {
       e.preventDefault();
