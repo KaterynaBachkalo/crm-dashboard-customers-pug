3a780be21307a11ce34f1c520079f1a57950dac3
KaterynaBachkalo <kozlovets88@gmail.com>
2024-10-09 12:01:42 +0300

    fix

--- src/js/sidebar-menu.js
+++ src/js/sidebar-menu.js
@@ -1,11 +1,13 @@
 const menuItems = document.querySelectorAll(".menu-link");
 
 const currentPath = window.location.pathname; 
+console.log("currentPath:", currentPath);
 
 const checkCurrentMenuLink = () => {
   menuItems.forEach((item) => {
 
     const clickPath = item.getAttribute("href").replace('./', '/');
+    console.log("clickPath1:", clickPath);
 
     if (clickPath === currentPath) {
       item.classList.add("current"); 
@@ -20,6 +22,7 @@ document.addEventListener("DOMContentLoaded", checkCurrentMenuLink);
 menuItems.forEach((item) => {
   item.addEventListener("click", (e) => {   
     const clickPath = item.getAttribute("href").replace('./', '/');
+    console.log("clickPath2:", clickPath);
 
     if (currentPath === clickPath) {
       e.preventDefault();
